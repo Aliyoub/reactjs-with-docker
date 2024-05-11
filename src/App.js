e0be1cd4963a3bb7d1800f982ecc1d526b6889e4
aliyou <aliyou@gmail.com>
2024-05-11 07:55:18 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        "Dockerisation" d'une application reactjs
+      </h1>
+    </div>
+  );
+}
+
+export default App;
